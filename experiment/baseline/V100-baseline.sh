2e5e7412061cfdf4a5a7d0e63a85289765e76a19
jiayongovo <li18145606780@163.com>
2024-05-18 18:38:02 +0800

    完成V100上的测量

--- experiment/baseline/V100-baseline.sh
+++ experiment/baseline/V100-baseline.sh
@@ -2,5 +2,5 @@
 
 for i in 14 16 18 20 22 24 26;
 do
-   BENCH_NPOW=$i cargo bench >> experiment/baseline/result/V100/jy-msm.txt
+   BENCH_NPOW=$i cargo bench | grep time: >> experiment/baseline/result/V100/jy-msm.txt
 done
