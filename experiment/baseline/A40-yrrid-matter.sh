410d299629819b8447f0a84091f3822a6b62c5c9
jiayongovo <li18145606780@163.com>
2024-05-18 21:10:40 +0800

    完成A40上yrrid和jy-msm的测量，随机标量

--- experiment/baseline/A40-yrrid-matter.sh
+++ experiment/baseline/A40-yrrid-matter.sh
@@ -1,8 +1,8 @@
 # 修改为377曲线
 
 # rm experiment/baseline/result/A40/jy-msm21-26.txt
-# rm experiment/baseline/result/A40/yrrid_21-26.txt
-# rm experiment/baseline/result/A40/matter_lab_21-26.txt
+rm experiment/baseline/result/A40/yrrid_21-26.txt
+rm experiment/baseline/result/A40/matter_lab_21-26.txt
 
 # for m in $(seq 1 3)
 # do 
@@ -18,7 +18,7 @@ for m in $(seq 1 3)
 do 
    for i in $(seq 21 26)
    do
-      BENCH_NPOW=$i cargo bench | grep 'time:   [' >> ../result/A40/yrrid_21-26.txt
+      BENCH_NPOW=$i cargo bench | grep "time:   \[" >> ../result/A40/yrrid_21-26.txt
    done
 done 
 
@@ -27,6 +27,6 @@ for m in $(seq 1 3)
 do 
    for i in $(seq 21 26)
    do
-      BENCH_NPOW=$i cargo bench | grep 'time:   [' >> ../result/A40/matter_lab_21-26.txt
+      BENCH_NPOW=$i cargo bench | grep time: >> ../result/A40/matter_lab_21-26.txt
    done
 done 
